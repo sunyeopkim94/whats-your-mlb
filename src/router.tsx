71a7f237c32ba002eefdb3fc30a0f86a2e6b80f4
Balenciaga <sunyeopkim94@naver.com>
2024-04-30 20:31:25 +0900

    add basename at BrowserRouter

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ interface IRouterProps {
 
 function Router({}:IRouterProps){
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route path="/:teamId">
                     <Team />
